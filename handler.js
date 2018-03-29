796486f7e29f2d3d58f980c24a27e6d2186c1a6d
Gurjeet Singh <gv9810@gmail.com>
2018-03-29 01:34:55 +0000

    first commit

--- handler.js
+++ handler.js
@@ -6,6 +6,10 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const accountSid = 'AC3aed2e0326600b357af83468f0e2bf9f'
 const authToken = '37e879c49802265455c0779a988e1b54'
 const client = require('twilio')(accountSid,authToken);
+const lambda = new AWS.Lambda({
+  region: 'us-east-1'
+})
+
 module.exports.filesaveinfo = (event) => {
   event.Records.forEach((record) => {
       console.log(record);
@@ -28,20 +32,17 @@ module.exports.filesaveinfo = (event) => {
             console.log(error);
         }
         console.log("success");
-  });
-  client.messages.create(
-  {
-    to: '+17783028501',
-    from: '+17784006904',
-    body: 'New file ' +filename+ ' has created.',
-  },
-  (err, message) => {
-      if(err){
-          
+        let params2 = {
+        FunctionName: 'first-serverless-dev-sendsms',
+        Payload: JSON.stringify({ filename: filename })
       }
-    console.log(message.sid)
-  }
-)
+      lambda.invoke(params2, (err, data) => {
+        if(err) {
+          console.log(err)
+          return
+        }
+})
+  });
 });
 }
 
@@ -68,4 +69,26 @@ module.exports.filedelinfo = (event) => {
   });
 });
 }
+
+
+module.exports.sendsms = (event, context, callback) => {
+client.messages.create(
+    {
+      to: '+17783028501',
+      from: '+17784006904',
+      body: `New file ${event.filename} has added to S3.`
+    },
+    (err, message) => {
+        if(err){
+            
+        }
+      console.log(message.sid)
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Success" }),
+      }
+      callback(null, response);
+    }
+  )
+};
 
